3e06a6351728aea2b6e9d3196af8423729258222
tomtom815 <98099798+tomtom815@users.noreply.github.com>
2022-03-18 15:44:20 -0500

    direct copy from discord

--- config/Database.php
+++ config/Database.php
@@ -1,31 +1,35 @@
 <?php
     class Database{
         // DB Params
-        $url = getenv('JAWSDB_URL');
-        $dbparts = parse_url($url);
-        
-        $hostname = $dbparts['host'];
-        $username = $dbparts['user'];
-        $password = $dbparts['pass'];
-        $database = ltrim($dbparts['path'],'/');
+
+        private $conn;
 
 
         // DB connect
         public function connect(){
-            $this->conn = null;
-
+            
+            $url = getenv('JAWSDB_URL');
+            $dbparts = parse_url($url);
+        
+            $hostname = $dbparts['host'];
+            $username = $dbparts['user'];
+            $password = $dbparts['pass'];
+            $database = ltrim($dbparts['path'],'/');
+            //You cannot do the above for your local dev environment, just Heroku
+        
+            // Create your new PDO connection here
+            // This is also from the Heroku docs showing the PDO connection: 
             try {
-                $conn = new PDO("mysql:host=$hostname;dbname=$database", $username, $password);
-                // set the PDO error mode to exception
-                $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-                echo "Connected successfully";
-                }
+            $conn = new PDO("mysql:host=$hostname;dbname=$database", $username, $password);
+              // set the PDO error mode to exception
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            echo "Connected successfully";
+            }
             catch(PDOException $e)
-                {
-                echo "Connection failed: " . $e->getMessage();
-                }
-            return $this->conn;
-
+            {
+            echo "Connection failed: " . $e->getMessage();
+            }
+    
         }
     }
 ?>
