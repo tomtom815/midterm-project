707f059796a4d4c070d45afc1e8d3488cb53e656
tomtom815 <98099798+tomtom815@users.noreply.github.com>
2022-03-18 15:34:22 -0500

    removed url variable

--- config/Database.php
+++ config/Database.php
@@ -1,10 +1,6 @@
 <?php
     class Database{
         // DB Params
-        
-        $url = getenv('JAWSDB_URL');
-        $dbparts = parse_url($url);
-
         private $host = 'x8autxobia7sgh74.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
         private $username = 'vys4c4kcbifex17h';
         private $password = 'seye1av40qv361an';
