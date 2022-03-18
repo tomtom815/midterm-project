c5e65b706db0d04aff1682481b4326ad8a9ce55f
tomtom815 <98099798+tomtom815@users.noreply.github.com>
2022-03-18 17:08:10 -0500

    again

--- config/Database.php
+++ config/Database.php
@@ -1,25 +1,34 @@
 <?php
-    class Database{
-        // DB Params
-        private $host = "x8autxobia7sgh74.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
-        private $db_name = "jqllxwqtcfrc3a98";
-        private $username = "vys4c4kcbifex17h";
-        private $password = "seye1av40qv361an";
+    class Database {
+        // Define the class properties here
+        // private $conn; for example 
         private $conn;
 
+        public function connect() {
+          // if creating a Heroku connection, this is straight from the dev center link: 
+        $url = getenv('JAWSDB_URL');
+        $dbparts = parse_url($url);
 
-        // DB connect
-        public function connect(){
-            $this->conn = null;
+        $hostname = $dbparts['host'];
+        $username = $dbparts['user'];
+        $password = $dbparts['pass'];
+        $database = ltrim($dbparts['path'],'/');
+          //You cannot do the above for your local dev environment, just Heroku
 
-            try{
-                $this->conn = new PDO('mysql:host =' . $this->host . ';dbname=' . $this->db_name, $this->username, $this->password);
-                $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            }catch(PDOException $e){
-                echo 'Connection Error: ' . $e->getMessage();
-            }
-            return $this->conn;
-
+          // Create your new PDO connection here
+          // This is also from the Heroku docs showing the PDO connection: 
+        try {
+            $this->conn = new PDO("mysql:host=$hostname;dbname=$database", $username, $password);
+            // set the PDO error mode to exception
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            echo "Connected successfully";
+}
+catch(PDOException $e)
+{
+            echo "Connection failed: " . $e->getMessage();
+}
+          // We used this PDO connection format in previous weeks - reference w3schools.com
         }
-    }
+} 
+
 ?>
