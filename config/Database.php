4919c7eaddd3008a574ae6ede8f69d93c5130cfc
tomtom815 <98099798+tomtom815@users.noreply.github.com>
2022-03-18 15:39:02 -0500

    back to jaws

--- config/Database.php
+++ config/Database.php
@@ -1,23 +1,29 @@
 <?php
     class Database{
         // DB Params
-        private $host = 'x8autxobia7sgh74.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
-        private $username = 'vys4c4kcbifex17h';
-        private $password = 'seye1av40qv361an';
-        private $db_name = 'jqllxwqtcfrc3a98';    
-        private $conn;
+        $url = getenv('JAWSDB_URL');
+        $dbparts = parse_url($url);
+        
+        $hostname = $dbparts['host'];
+        $username = $dbparts['user'];
+        $password = $dbparts['pass'];
+        $database = ltrim($dbparts['path'],'/');
 
 
         // DB connect
         public function connect(){
             $this->conn = null;
 
-            try{
-                $this->conn = new PDO('mysql:host =' . $this->host . ';dbname=' . $this->db_name, $this->username, $this->password);
-                $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            }catch(PDOException $e){
-                echo 'Connection Error: ' . $e->getMessage();
-            }
+            try {
+                $conn = new PDO("mysql:host=$hostname;dbname=$database", $username, $password);
+                // set the PDO error mode to exception
+                $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                echo "Connected successfully";
+                }
+            catch(PDOException $e)
+                {
+                echo "Connection failed: " . $e->getMessage();
+                }
             return $this->conn;
 
         }
