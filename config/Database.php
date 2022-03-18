e548dad95f8875f198627737ca5e8395bdd20ab8
tomtom815 <98099798+tomtom815@users.noreply.github.com>
2022-03-18 16:17:10 -0500

    still trying

--- config/Database.php
+++ config/Database.php
@@ -2,28 +2,19 @@
     class Database{
         // DB Params
         
-        private $hostname;
-        private $username;
-        private $password;
-        private $database;
+        private $hostname = 'x8autxobia7sgh74.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
+        private $username = 'vys4c4kcbifex17h';
+        private $password = getenv("PASS");
+        private $database = 'jqllxwqtcfrc3a98';
         private $conn;
 
 
         // DB connect
         public function connect() {
-            // if creating a Heroku connection, this is straight from the dev center link: 
-        
-        
-            $this->hostname = 'x8autxobia7sgh74.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
-            $this->username = 'vys4c4kcbifex17h';
-            $this->password = getenv("PASS");
-            $this->database = 'jqllxwqtcfrc3a98';
-            //You cannot do the above for your local dev environment, just Heroku
-        
             // Create your new PDO connection here
             // This is also from the Heroku docs showing the PDO connection: 
             try {
-                $this->conn = new PDO("mysql:host=$hostname;dbname=$database", $username, $password);
+                $this->conn = new PDO("mysql:host=$this->hostname;dbname=$this->database", $this->username, $this->password);
               // set the PDO error mode to exception
                 $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
                 echo "Connected successfully";
