43e69c9b9cc2c8dd746433d4e9f43439ae0df447
tomtom815 <98099798+tomtom815@users.noreply.github.com>
2022-03-19 07:44:21 -0500

    directly entered password

--- config/Database.php
+++ config/Database.php
@@ -6,7 +6,7 @@
 
         private $hostname = "x8autxobia7sgh74.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
         private $username = "vys4c4kcbifex17h";
-        private $password = getenv("PASS");
+        private $password = "tbtixrj0pak8ol19";
         private $database = "jqllxwqtcfrc3a98";
         private $conn;
 
