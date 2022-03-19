ef16281bbbb0644a73aecff00c094e6a291b7dfe
tomtom815 <98099798+tomtom815@users.noreply.github.com>
2022-03-18 20:19:12 -0500

    tweaked pdo again

--- config/Database.php
+++ config/Database.php
@@ -13,12 +13,16 @@
         public function connect(){
             $this->conn = null;
 
-            try{
-                $this->conn = new mysqli($this->hostname, $this->username, $this->password, $this->database);
+            try {
+                $this->conn = new PDO("mysql:host=$this->hostname;dbname=$this->database", $this->username, $this->password);
+                // set the PDO error mode to exception
                 $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            }catch(PDOException $e){
-                echo 'Connection Error: ' . $e->getMessage();
-            }
+                echo "Connected successfully";
+                }
+            catch(PDOException $e)
+                {
+                echo "Connection failed: " . $e->getMessage();
+                }
             return $this->conn;
 
         }
