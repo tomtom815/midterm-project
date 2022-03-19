fb0bc985a52c625a18870bb0c5fc5943d913cf85
tomtom815 <98099798+tomtom815@users.noreply.github.com>
2022-03-18 19:30:26 -0500

    Exact copy from dev center

--- config/Database.php
+++ config/Database.php
@@ -4,30 +4,24 @@
     // private $conn; for example 
 
     public function connect() {
-      // if creating a Heroku connection, this is straight from the dev center link: 
-        /*$url = getenv('JAWSDB_URL');
+        $url = getenv('JAWSDB_URL');
         $dbparts = parse_url($url);
-
+        
         $hostname = $dbparts['host'];
         $username = $dbparts['user'];
         $password = $dbparts['pass'];
-        $database = ltrim($dbparts['path'],'/');*/
+        $database = ltrim($dbparts['path'],'/');
+        $conn;
 
       // Create your new PDO connection here
       // This is also from the Heroku docs showing the PDO connection: 
-        try {
-        $conn = new PDO("mysql://vys4c4kcbifex17h:seye1av40qv361an@x8autxobia7sgh74.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/jqllxwqtcfrc3a98");
-        // set the PDO error mode to exception
-        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        echo "Connected successfully";
-        }
-        catch(PDOException $e)
-        {
-        echo "Connection failed: " . $e->getMessage();
-        }
-      // We used this PDO connection format in previous weeks - reference w3schools.com
-    }
-        } 
+        // Create connection
+        $this->conn = new mysqli($hostname, $username, $password, $database);
 
+        // Check connection
+        if ($this->conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        }
+        echo "Connection was successfully established!";
     
 ?>
