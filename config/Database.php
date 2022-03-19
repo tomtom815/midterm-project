b9ff4a80a0dcbc5f71fe5b80c09e5986c2a0923d
tomtom815 <98099798+tomtom815@users.noreply.github.com>
2022-03-18 21:27:17 -0500

    missed ';'

--- config/Database.php
+++ config/Database.php
@@ -7,7 +7,7 @@
         private $hostname = "x8autxobia7sgh74.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
         private $username = "vys4c4kcbifex17h";
         private $password = getenv("PASS");
-        private $database = "jqllxwqtcfrc3a98"
+        private $database = "jqllxwqtcfrc3a98";
         private $conn;
 
         public function connect(){
