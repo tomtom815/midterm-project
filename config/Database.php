287d9eaa7f93c965e7407b2c1d5fce282f308623
tomtom815 <98099798+tomtom815@users.noreply.github.com>
2022-03-18 21:22:50 -0500

    placing in everything but pass directly to vars

--- config/Database.php
+++ config/Database.php
@@ -1,20 +1,20 @@
 <?php
     class Database{
         // DB Params
-        private $url = getenv('JAWSDB_URL');
-        private $dbparts = parse_url($url);
+        /*private $url = getenv('JAWSDB_URL');
+        private $dbparts = parse_url($url);*/
 
-        private $hostname = $dbparts['host'];
-        private $username = $dbparts['user'];
-        private $password = $dbparts['pass'];
-        private $database = ltrim($dbparts['path'],'/');
+        private $hostname = "x8autxobia7sgh74.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+        private $username = "vys4c4kcbifex17h";
+        private $password = getenv("PASS");
+        private $database = "jqllxwqtcfrc3a98"
         private $conn;
 
         public function connect(){
             $this->conn = null;
 
             try {
-                $this->conn = new PDO("mysql:host=$this->hostname;dbname=$this->database", $this->username, $this->password);
+                $this->conn = new PDO("mysql:host="$this->hostname";dbname="$this->database", $this->username, $this->password");
                 // set the PDO error mode to exception
                 $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
                 echo "Connected successfully";
