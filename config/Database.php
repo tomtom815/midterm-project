bbf2d35b51f26ca1d61f34779ed7fafafa083cc6
tomtom815 <98099798+tomtom815@users.noreply.github.com>
2022-03-18 19:49:56 -0500

    added private

--- config/Database.php
+++ config/Database.php
@@ -1,13 +1,13 @@
 <?php
     class Database{
         // DB Params
-        $url = getenv('JAWSDB_URL');
-        $dbparts = parse_url($url);
+        private $url = getenv('JAWSDB_URL');
+        private $dbparts = parse_url($url);
 
-        $hostname = $dbparts['host'];
-        $username = $dbparts['user'];
-        $password = $dbparts['pass'];
-        $database = ltrim($dbparts['path'],'/');
+        private $hostname = $dbparts['host'];
+        private $username = $dbparts['user'];
+        private $password = $dbparts['pass'];
+        private $database = ltrim($dbparts['path'],'/');
         private $conn;
 
         public function connect(){
