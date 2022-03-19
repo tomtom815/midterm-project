60fa90522fc233daed1bd6445315780352ab1b1e
tomtom815 <98099798+tomtom815@users.noreply.github.com>
2022-03-19 07:46:55 -0500

    removed connected successfully

--- config/Database.php
+++ config/Database.php
@@ -17,7 +17,7 @@
                 $this->conn = new PDO("mysql:host=$this->hostname;dbname=$this->database", $this->username, $this->password);
                 // set the PDO error mode to exception
                 $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-                echo "Connected successfully";
+                
                 }
             catch(PDOException $e)
                 {
